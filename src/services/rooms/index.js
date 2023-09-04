b623c54172466f6bfd56bcf791ecebab35b07807
Siddharth <siddharth190820@gmail.com>
2023-09-04 13:16:36 +0530

    feat(search v1): opt slot searching

--- src/services/rooms/index.js
+++ src/services/rooms/index.js
@@ -67,20 +67,74 @@ const searchRooms = async (from, to) => {
     const fromDate = new Date(from);
     const toDate = new Date(to);
     const roomsOccupancy = await getRoomsOccupancy();
+
+    const whereClause = 
+  {
+    // 10 - 11
+    [db.Sequelize.Op.or]: [
+      // case 1: 10:15 - 10:30
+      {
+        from: {
+          [db.Sequelize.Op.lt]: toDate,
+          [db.Sequelize.Op.lt]: fromDate
+        },
+        to: {
+          [db.Sequelize.Op.gt]: fromDate,
+          [db.Sequelize.Op.gt]: toDate
+        }
+      },
+      // case 2: 9 to 12
+      {
+        from: {
+          [db.Sequelize.Op.lt]: toDate,  
+          [db.Sequelize.Op.gt]: fromDate
+        },
+        to: {
+          [db.Sequelize.Op.lt]: fromDate,
+          [db.Sequelize.Op.lt]: toDate 
+        }
+      },
+      // case 3: 9 to 10:30
+      {
+        from: {
+          [db.Sequelize.Op.lt]: toDate,
+          [db.Sequelize.Op.gt]: fromDate
+        },
+        to: {
+          [db.Sequelize.Op.gt]: fromDate,
+          [db.Sequelize.Op.gt]: toDate 
+        }
+      },
+      // case 4: 10:30 to 12
+      {
+        from: {
+          [db.Sequelize.Op.lt]: toDate,
+          [db.Sequelize.Op.lt]: fromDate
+        },
+        to: {
+          [db.Sequelize.Op.gt]: fromDate,
+          [db.Sequelize.Op.lt]: toDate
+        }
+      },
+      // addn cases
+      {
+        from: {
+          [db.Sequelize.Op.lt]: toDate,
+          [db.Sequelize.Op.gt]: fromDate
+        }
+      },
+      {
+        to: {
+          [db.Sequelize.Op.lt]: toDate,
+          [db.Sequelize.Op.gt]: fromDate
+        }
+      }
+    ]
+  };
+    
     const bookedRooms = await db.Event.findAll({
       attributes: ['roomNo'],
-      where: {
-        [db.Sequelize.Op.or]: [
-          {
-            from: {
-              [db.Sequelize.Op.lt]: toDate,
-            },
-            to: {
-              [db.Sequelize.Op.gt]: fromDate,
-            },
-          },
-        ],
-      },
+      where: whereClause,
       raw: true,
     });
 
